657c8d500e01492f37a10252d517c931f726c3f8
quin345 <jcdgingo@gmail.com>
2026-09-01 07:26:27 +0800

    used default/Files to save the file

--- fetch_asset_metadata.SparkJobDefinition/Main/fetch_asset_metadata.py
+++ fetch_asset_metadata.SparkJobDefinition/Main/fetch_asset_metadata.py
@@ -106,7 +106,7 @@ def parse_args():
     parser.add_argument(
         "--output",
         type=Path,
-        default=Path("Files/asset_metadata.json"),
+        default=Path("default/Files/asset_metadata.json"),
         help="JSON file for the complete metadata dump.",
     )
     parser.add_argument(
